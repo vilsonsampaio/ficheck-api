ac3f9954a751023deb55c58bd00201554b1d3503
VilsonSampaio <vilson.sampaio04@gmail.com>
2023-12-22 22:19:24 -0300

    feat: using database module to centralize repositories

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { UsersRepository } from 'src/shared/database/repositories/users.repositories';
-import { PrismaService } from 'src/shared/database/prisma.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, UsersRepository, PrismaService],
+  providers: [UsersService],
 })
 export class UsersModule {}
