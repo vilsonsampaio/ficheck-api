f3348765b2353b9bfc1084c9c3203c78405a991d
VilsonSampaio <vilson.sampaio04@gmail.com>
2023-12-22 21:41:01 -0300

    feat: create user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import { PrismaService } from 'src/database/prisma.service';
+import { hash } from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return createUserDto;
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const { name, email, password } = createUserDto;
+
+    const isEmailAlreadyExists = await this.prismaService.user.findUnique({
+      where: { email },
+    });
+
+    if (isEmailAlreadyExists) {
+      throw new ConflictException('This email is already in use');
+    }
+
+    const hashedPassword = await hash(password, 12);
+
+    const user = await this.prismaService.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    return user;
   }
 }
